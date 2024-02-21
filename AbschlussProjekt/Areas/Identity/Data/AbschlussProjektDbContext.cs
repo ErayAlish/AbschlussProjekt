1c4a0399ed9083887f012af08a3c17a268ca0a0b
Bin Aktiv Du Passiv ☯︎︎馬 <erayalish8848@gmail.com>
2024-02-21 22:57:39 +0100

    Add project files.

---
+++ AbschlussProjekt/Areas/Identity/Data/AbschlussProjektDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AbschlussProjekt.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbschlussProjekt.Data;
+
+public class AbschlussProjektDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AbschlussProjektDbContext(DbContextOptions<AbschlussProjektDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
